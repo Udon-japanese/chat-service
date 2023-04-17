1f440074e0abfe814edf202780820aa74cf6af60
Udon-japanese <odaniyoshito@gmail.com>
2023-04-17 14:48:12 +0900

    aタグからlinkタグに変更

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <a href="/chat" className="dark:text-white hover:text-blue-400 hover:underline">ここからチャットしよう</a>
+    <Link href="/chat" className="dark:text-white hover:text-blue-400 hover:underline">ここからチャットしよう</Link>
   );
 }
